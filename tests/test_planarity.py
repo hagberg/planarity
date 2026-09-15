591da48f8a634ad8aa0759d1932d797ad26ad125
Vitor <vitorchassot456@gmail.com>
2026-09-14 21:35:14 -0300

    Add writeMode parameter to write functions and add write_tests example

--- tests/test_planarity.py
+++ tests/test_planarity.py
@@ -121,3 +121,34 @@ class TestPlanarity:
         answer = 'N=2\n1: 2 0\n2: 1 0\n'
         assert d == answer
         os.unlink(fname)
+
+    def test_write_adjmatrix(self):
+        e = ([1,2],)
+        P = planarity.PGraph(e)
+        fname = tempfile.mktemp()
+        P.write(fname, planarity.WRITE_ADJMATRIX)
+        d = open(fname).read()
+        answer = '2\n 1\n  \n'
+        assert d == answer
+        os.unlink(fname)
+
+    def test_write_g6(self):
+        e = ([1,2],)
+        P = planarity.PGraph(e)
+        fname = tempfile.mktemp()
+        P.write(fname, planarity.WRITE_G6)
+        d = open(fname).read()
+        answer = '>>graph6<<A_\n'
+        assert d == answer
+        os.unlink(fname)
+
+    def test_write_function_g6(self):
+        e = ([1,2],)
+        fname = tempfile.mktemp()
+        planarity.write(e, fname, planarity.WRITE_G6)
+        d = open(fname).read()
+        answer = '>>graph6<<A_\n'
+        assert d == answer
+        os.unlink(fname)
+
+
